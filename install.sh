8b067f1d9b1dd26b62f0202ca268809ca79e72d0
defunsm <salmanhossain500@gmail.com>
2020-07-07 00:01:39 -0400

    zsh

--- install.sh
+++ install.sh
@@ -73,11 +73,11 @@ function install_config {
     sudo ln -sf "$PWD"/.doom.d/config.el ~/.doom.d/config.el
     sudo ln -sf "$PWD"/.doom.d/init.el ~/.doom.d/init.el
     sudo ln -sf "$PWD"/.doom.d/packages.el ~/.doom.d/packages.el
-    sudo ln -sf "$PWD"/zsh /etc/share/zsh
+    sudo linkDir -sf "$PWD"/zsh /etc/share/zsh
 }
 
 function install_extra {
-    yay -S virt-manager hddtemp glxinfo hardinfo lm-sensors vlc rt-manager hddtemp glxinfo hardinfo lm-sensors vlc stacer
+    yay -S virt-manager hddtemp glxinfo hardinfo lm-sensors vlc hddtemp glxinfo hardinfo vlc stacer
 }
 
 initialize
